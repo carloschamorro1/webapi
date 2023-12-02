a2e1fc1a5c0f1c573ea9a4dc3df6d4cc23e7edc7
Carlos Mauricio Chamorro Torres <cmchamorro99@hotmail.com>
2023-12-02 10:43:58 -0600

    Add project files.

---
+++ webapi/Services/HelloWorldService.cs
@@ -0,0 +1,16 @@
+﻿namespace webapi.Services
+{
+    public class HelloWorldService : IHelloWorldService
+    {
+        public string GetHelloWorld()
+        {
+            return "Hello World!";
+        }
+
+    }
+
+    public interface IHelloWorldService
+    {
+        string GetHelloWorld();
+    }
+}
